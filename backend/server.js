e2c6e00e9521e6691b17ed7c29ffba5ab0bba563
SheikhArman100 <sheikharman100@gmail.com>
2024-01-20 18:10:16 +0600

    backend small change

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,9 @@ app.use(
 );
 
 //routes
+app.get("/api/",(req,res)=>{
+  res.json({message:"API is working"})
+})
 app.use("/api/auth", authRouter);
 app.use("/api/user",userRouter)
 app.use("/api/task",taskRoute)
